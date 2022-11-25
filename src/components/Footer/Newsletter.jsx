589e237bca65f1a2244456ffd7248d78dae51d05
EnzoAMarco <enzom945@gmail.com>
2022-11-25 15:09:04 -0300

    fin find us

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import SubHeading from '../SubHeading/SubHeading';
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newsletter'>
+    <div className='app__newsletter-heading'>
+      <SubHeading title='Newsletter' />
+      <h1 className='headtext__cormorant'> Subscribite a nuestro diario</h1>
+    </div>
   </div>
 );
 
--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images, data } from '../../constants';
+
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className='app__bg app__wrapper section__padding' id='contact'>
+
+    <div className='app__wrapper_info'>
+      <SubHeading title='Contacto' />
+      <h1 className='headtext__cormorant' style={{marginBottom: '3rem'}} >Donde encontrarnos</h1>
+      <div className='app__wrapper-content'>
+        <p className='p__opensans'>ESCRIBIR DIRECCION</p>
+        <p className='p__opensans' style={{color: '#DCCA87', margin:'2rem 0'}}>Horarios</p>
+        <p className='p__opensans'>Lun - vie: 10:00AM - 01:00AM</p>
+        <p className='p__opensans'>Sab - Dom: 11:00AM - 02:00AM</p>
+      </div>
+      <button type='button' className='custom__button' style={{marginTop: '2rem'}}>Visitanos</button>
+    </div>
+
+    <div className='app__wrapper_img'>
+      <img src={images.findus} alt='bebida img' />
+    </div>
+
   </div>
 );
 
