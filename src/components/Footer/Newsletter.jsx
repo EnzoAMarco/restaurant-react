571fef64e07d514650210b83d81d36d46b9515dc
EnzoAMarco <enzom945@gmail.com>
2022-11-25 16:48:12 -0300

    limpiar bugs

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -5,10 +5,18 @@ import './Newsletter.css';
 
 const Newsletter = () => (
   <div className='app__newsletter'>
+
     <div className='app__newsletter-heading'>
       <SubHeading title='Newsletter' />
-      <h1 className='headtext__cormorant'> Subscribite a nuestro diario</h1>
+      <h1 className='headtext__cormorant'> Suscribete a nuestro diario</h1>
+      <p className='p__opensans'>Para nunca perderte las novedades!</p>
     </div>
+
+    <form className='app__newsletter-input flex__center'>
+      <input className='app__newsletter-input_email' type='email' placeholder='Escriba su dirección e-email' />
+      <input type='submit' style={{width:'max-content'}}className='custom__button' value='Suscribite'/>
+    </form>
+
   </div>
 );
 
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi'
+import { MdCopyright } from 'react-icons/md'
 
+import { images } from '../../constants'
 import { FooterOverlay, Newsletter } from '../../components';
 import './Footer.css';
 
@@ -8,6 +10,41 @@ const Footer = () => (
   <div className='app__footer section__padding'>
     <FooterOverlay />
     <Newsletter />
+    <div className='app__footer-links'>
+
+      <div className='app__footer-links_contact'>
+        <h1 className='app__footer-headtext'>Contactanos</h1>
+        <p className='p__opensans'>Macatai Güemez 154, Buenos Aires, Z7009FAK CABA, Argentina</p>
+        <p className='p__opensans'>+54 911 9330 6813</p>
+        <p className='p__opensans'>+54 911 7230 1007</p>
+      </div>
+
+      <div className='app__footer-links_logo'>
+        <h1 className='app__restaurant_title'>Zunpíer</h1>
+        <p className='p__opensans'>"Una experiencia inolvidable e unica solo en Zunpíer"</p>
+        <img src={images.spoon} alt='cuchara' style={{marginTop: '15px'}} />
+        <div className='app__footer-links_icons'>
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+
+      <div className='app__footer-links_work'>
+        <h1 className='app__footer-headtext'>Horario de atención</h1>
+        <p className='p__opensans'>Lun - Vie:</p>
+        <p className='p__opensans'>08:00AM - 10:00PM</p>
+        <p className='p__opensans'>Sab - Dom:</p>
+        <p className='p__opensans'>10:00AM - 10:00PM</p>
+      </div>
+
+    </div>
+    <div className='footer__copyright'>
+      <p className='p__opensans'> 
+      <MdCopyright />
+        2022 Zunpíer. Todos los derechos reservados
+      </p>
+    </div>
   </div>
 );
 
